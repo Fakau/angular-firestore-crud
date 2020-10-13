ea02df710033e2df2f33bc327039f41c9251d58d
Laurent Kafka M <laurentkafka123@gmail.com>
2020-10-13 11:41:02 -0400

    add angular dependencies

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: 'AIzaSyDKj16zuUdY-oM1QH0gilcsx-_8dw-21e0',
+    authDomain: 'angular-firestore-ed482.firebaseapp.com',
+    databaseURL: 'https://angular-firestore-ed482.firebaseio.com',
+    projectId: 'angular-firestore-ed482',
+    storageBucket: 'angular-firestore-ed482.appspot.com',
+    messagingSenderId: '1046545238409',
+    appId: '1:1046545238409:web:718ca706a75d4f806fb958',
+    measurementId: 'G-X4DTHCS5Y8'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyDKj16zuUdY-oM1QH0gilcsx-_8dw-21e0',
+    authDomain: 'angular-firestore-ed482.firebaseapp.com',
+    databaseURL: 'https://angular-firestore-ed482.firebaseio.com',
+    projectId: 'angular-firestore-ed482',
+    storageBucket: 'angular-firestore-ed482.appspot.com',
+    messagingSenderId: '1046545238409',
+    appId: '1:1046545238409:web:718ca706a75d4f806fb958',
+    measurementId: 'G-X4DTHCS5Y8'
+  }
 };
 
 /*
